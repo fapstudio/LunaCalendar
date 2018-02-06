6e40acd26f73b004e57f8182b273edaa421c5736
Hue Bui <hbui20@csc.com>
2018-02-06 15:27:11 +0700

    FA-001 Add calendar!

--- sourcecode/FapCalendar/App.js
+++ sourcecode/FapCalendar/App.js
@@ -5,6 +5,7 @@
  */
 
 import React, { Component } from 'react';
+import {Calendar} from 'react-native-calendars';
 import {
   Platform,
   StyleSheet,
@@ -21,18 +22,31 @@ const instructions = Platform.select({
 
 export default class App extends Component<{}> {
   render() {
+	 
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+		<Calendar
+		  // Specify theme properties to override specific styles for calendar parts. Default = {}
+		  theme={{
+			backgroundColor: '#ffffff',
+			calendarBackground: '#ffffff',
+			textSectionTitleColor: '#b6c1cd',
+			selectedDayBackgroundColor: '#00adf5',
+			selectedDayTextColor: '#ffffff',
+			todayTextColor: '#00adf5',
+			dayTextColor: '#2d4150',
+			textDisabledColor: '#d9e1e8',
+			dotColor: '#00adf5',
+			selectedDotColor: '#ffffff',
+			arrowColor: 'orange',
+			monthTextColor: 'blue',
+			textDayFontFamily: 'monospace',
+			textMonthFontFamily: 'monospace',
+			textDayHeaderFontFamily: 'monospace',
+			textDayFontSize: 16,
+			textMonthFontSize: 16,
+			textDayHeaderFontSize: 16
+		  }}
+		/>
     );
   }
 }
